53962e3aab11744e3a6e48bd204a6988808f5490
felixfixo <fixoothina@gmail.com>
2022-06-15 18:02:50 +0300

    changes

--- shell.c
+++ shell.c
@@ -1,150 +1,89 @@
-#include "holberton.h"
+#include "shell.h"
 
 /**
- * shell - simple shell
- * @build: input build
+ * sig_handler - checks if Ctrl C is pressed
+ * @sig_num: int
  */
-void shell(config *build)
+void sig_handler(int sig_num)
 {
-	while (true)
+	if (sig_num == SIGINT)
 	{
-		checkAndGetLine(build);
-		if (splitString(build) == false)
-			continue;
-		if (findBuiltIns(build) == true)
-			continue;
-		checkPath(build);
-		forkAndExecute(build);
+		_puts("\n$ ");
 	}
 }
 
 /**
- * checkAndGetLine - check stdin and retrieves next line; handles
- * prompt display
- * @build: input build
+* _EOF - handles the End of File
+* @len: return value of getline function
+* @buff: buffer
  */
-void checkAndGetLine(config *build)
+void _EOF(int len, char *buff)
 {
-	register int len;
-	size_t bufferSize = 0;
-	char *ptr, *ptr2;
-
-	build->args = NULL;
-	build->envList = NULL;
-	build->lineCounter++;
-	if (isatty(STDIN_FILENO))
-		displayPrompt();
-	len = getline(&build->buffer, &bufferSize, stdin);
-	if (len == EOF)
+	(void)buff;
+	if (len == -1)
 	{
-		freeMembers(build);
 		if (isatty(STDIN_FILENO))
-			displayNewLine();
-		if (build->errorStatus)
-			exit(build->errorStatus);
-		exit(EXIT_SUCCESS);
-
-	}
-	ptr = _strchr(build->buffer, '\n');
-	ptr2 = _strchr(build->buffer, '\t');
-	if (ptr || ptr2)
-		insertNullByte(build->buffer, len - 1);
-	stripComments(build->buffer);
-}
-
-/**
- * stripComments - remove comments from input string
- * @str: input string
- * Return: length of remaining string
- */
-void stripComments(char *str)
-{
-	register int i = 0;
-	_Bool notFirst = false;
-
-	while (str[i])
-	{
-		if (i == 0 && str[i] == '#')
 		{
-			insertNullByte(str, i);
-			return;
+			_puts("\n");
+			free(buff);
 		}
-		if (notFirst)
+		exit(0);
+	}
+}
+/**
+  * _isatty - verif if terminal
+  */
+
+void _isatty(void)
+{
+	if (isatty(STDIN_FILENO))
+		_puts("$ ");
+}
+/**
+ * main - Shell
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	ssize_t len = 0;
+	char *buff = NULL, *value, *pathname, **arv;
+	size_t size = 0;
+	list_path *head = '\0';
+	void (*f)(char **);
+
+	signal(SIGINT, sig_handler);
+	while (len != EOF)
+	{
+		_isatty();
+		len = getline(&buff, &size, stdin);
+		_EOF(len, buff);
+		arv = splitstring(buff, " \n");
+		if (!arv || !arv[0])
+			execute(arv);
+		else
 		{
-			if (str[i] == '#' && str[i - 1] == ' ')
+			value = _getenv("PATH");
+			head = linkpath(value);
+			pathname = _which(arv[0], head);
+			f = checkbuild(arv);
+			if (f)
 			{
-				insertNullByte(str, i);
-				return;
+				free(buff);
+				f(arv);
+			}
+			else if (!pathname)
+				execute(arv);
+			else if (pathname)
+			{
+				free(arv[0]);
+				arv[0] = pathname;
+				execute(arv);
 			}
 		}
-		i++;
-		notFirst = true;
 	}
-}
-
-/**
- * forkAndExecute - fork current build and execute processes
- * @build: input build
- */
-void forkAndExecute(config *build)
-{
-	int status;
-	pid_t f1 = fork();
-
-	convertLLtoArr(build);
-	if (f1 == -1)
-	{
-		perror("error\n");
-		freeMembers(build);
-		freeArgs(build->envList);
-		exit(1);
-	}
-	if (f1 == 0)
-	{
-		if (execve(build->fullPath, build->args, build->envList) == -1)
-		{
-			errorHandler(build);
-			freeMembers(build);
-			freeArgs(build->envList);
-			if (errno == ENOENT)
-				exit(127);
-			if (errno == EACCES)
-				exit(126);
-		}
-	} else
-	{
-		wait(&status);
-		if (WIFEXITED(status))
-			build->errorStatus = WEXITSTATUS(status);
-		freeArgsAndBuffer(build);
-		freeArgs(build->envList);
-	}
-}
-
-/**
- * convertLLtoArr - convert linked list to array
- * @build: input build
- */
-void convertLLtoArr(config *build)
-{
-	register int i = 0;
-	size_t count = 0;
-	char **envList = NULL;
-	linked_l *tmp = build->env;
-
-	count = list_len(build->env);
-	envList = malloc(sizeof(char *) * (count + 1));
-	if (!envList)
-	{
-		perror("Malloc failed\n");
-		exit(1);
-	}
-	while (tmp)
-	{
-		envList[i] = _strdup(tmp->string);
-		tmp = tmp->next;
-		i++;
-	}
-	envList[i] = NULL;
-	build->envList = envList;
+	free_list(head);
+	freearv(arv);
+	free(buff);
+	return (0);
 }
